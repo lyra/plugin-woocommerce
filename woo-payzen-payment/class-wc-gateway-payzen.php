e7e1b9fb7631b2fdf6d4b84d08f4a0dbf201a191
Nabil Berhouche <nabil.berhouche@lyra-network.com>
2024-07-01 16:44:11 +0200

    [BOT] Migrate hotfix 1.13.3 to GitHub.

--- woo-payzen-payment/class-wc-gateway-payzen.php
+++ woo-payzen-payment/class-wc-gateway-payzen.php
@@ -37,7 +37,7 @@ class WC_Gateway_Payzen extends WC_Payment_Gateway
 
     const CMS_IDENTIFIER = 'WooCommerce_2.x-8.x';
     const SUPPORT_EMAIL = 'support@payzen.eu';
-    const PLUGIN_VERSION = '1.13.2';
+    const PLUGIN_VERSION = '1.13.3';
     const GATEWAY_VERSION = 'V2';
 
     const LOG_FOLDER = 'wp-content/uploads/wc-logs/';
